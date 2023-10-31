4e36cd6fd1a109a2a599d6c19c49fba9af0aa2bd
Ariel Saldana <ariel@unreal.io>
2023-10-30 20:20:57 -0400

    Update discord_client to make use of the new gateway bot api to get the correct websocket url

--- src/client/discord_client.cpp
+++ src/client/discord_client.cpp
@@ -4,6 +4,7 @@
 
 #include "discord_client.hpp"
 #include "http-client/register_slash_command.h"
+#include "http-client/get_gateway_bot_api.h"
 #include "protocol/deserializer.h"
 #include "protocol/handlers/dispatch_handler.h"
 #include "websocket_client.hpp"
@@ -13,7 +14,26 @@
 discord_client::discord_client(std::string bot_token, const discord_intents &intents) {
     client_state.set_bot_token(std::move(bot_token));
     client_state.set_intents(intents);
-    client_state.set_ws_client(std::make_shared<websocket_client>(uri, hostname));
+    get_gateway_connection_info();
+    connect();
+}
+
+void discord_client::get_gateway_connection_info() {
+    auto gateway_bot_response_str = get_gateway_bot(client_state.get_bot_token());
+    std::cout << gateway_bot_response_str << std::endl;
+    gateway_connection_info.deserialize_string(gateway_bot_response_str);
+}
+
+bool discord_client::can_connect_to_gateway() {
+    return gateway_connection_info.getSessionStartLimit().getRemaining() != 0;
+}
+
+void discord_client::connect() {
+    if (!can_connect_to_gateway()) {
+        std::cerr << "Unable to connect, no attempts remaining" << std::endl;
+    }
+
+    client_state.set_ws_client(std::make_shared<websocket_client>(gateway_connection_info.getUrl()));
 
     event_handler_ = std::make_unique<event_handler>(this->client_state);
 
@@ -35,7 +55,6 @@ discord_client::discord_client(std::string bot_token, const discord_intents &int
         }
 
         handler->handle_event(gateway_event.d, gateway_event.t);
-        //std::visit(*handler, gateway_event.d);
     });
 
     client_state.get_ws_client()->connect();
--- src/client/discord_client.hpp
+++ src/client/discord_client.hpp
@@ -7,6 +7,7 @@
 
 #include "client/discord_client_state.h"
 #include "client/websocket_client.hpp"
+#include "models/gateway_bot.h"
 #include "protocol/handlers/event_handler.h"
 #include <iostream>
 #include <utility>
@@ -14,10 +15,12 @@
 class discord_client {
 private:
     typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
-    const std::string uri = "wss://gateway.discord.gg/?v=10&encoding=json";
-    const std::string hostname = "gateway.discord.gg";
     discord_client_state client_state{};
     std::unique_ptr<event_handler> event_handler_;
+    gateway_bot gateway_connection_info{};
+    void get_gateway_connection_info();
+    void connect();
+    bool can_connect_to_gateway();
 
 public:
     explicit discord_client(std::string bot_token, const discord_intents &intents);
