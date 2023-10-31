b85b9d196712418e18c2dad6cae0d4dd8fe218d9
Ariel Saldana <ariel@unreal.io>
2023-10-30 20:17:11 -0400

    Update deserializable class to include a deserialize_string method

--- src/protocol/deserializable.h
+++ src/protocol/deserializable.h
@@ -6,8 +6,17 @@
 #define DISCORDLITE_DESERIALIZABLE_H
 
 #include "rapidjson/document.h"
+#include <iostream>
+#include <string>
+
 struct deserializable {
     virtual void deserialize(const rapidjson::Value &param) = 0;
+
+    void deserialize_string(const std::string &json) {
+        rapidjson::Document d;
+        d.Parse(json.c_str());
+        return deserialize(d);
+    };
 };
 
 #endif//DISCORDLITE_DESERIALIZABLE_H
