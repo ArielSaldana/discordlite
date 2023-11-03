9c48f4995b04d6dd93286f90b1ec2f54228b3793
Ariel Saldana <ariel@unreal.io>
2023-11-02 22:41:19 -0400

    Add additional bugs to trace heartbeat race condition.

--- src/protocol/handlers/hello_handler.cpp
+++ src/protocol/handlers/hello_handler.cpp
@@ -43,17 +43,61 @@ void hello_handler::send_ready_event(const discord_client_state &client_state) c
     client_state.get_ws_client()->send_message(msg);
 }
 
-void hello_handler::start_heartbeat(const discord_client_state &client_state, int interval) const {
-    heartbeat_ = std::make_unique<heartbeat>(std::chrono::milliseconds(interval), [this, &client_state]() {
-        client_state.get_ws_client()->get_client()->get_alog().write(websocketpp::log::alevel::app, "Sequence: " + std::to_string(client_state.get_sequence_counter().value_or(0)));
-        if (client_state.is_client_connected_to_gateway()) {
-            auto s = client_state.get_sequence_counter() ? std::to_string(client_state.get_sequence_counter().value()) : "null";
-            std::stringstream ss;
-            ss << R"({"op": 1,"d": )" << s << R"(})";
-            client_state.get_ws_client()->send_message(ss.str());
-        } else {
+//void hello_handler::start_heartbeat(const discord_client_state &client_state, int interval) const {
+//    heartbeat_ = std::make_unique<heartbeat>(std::chrono::milliseconds(interval), [this, &client_state]() {
+//        client_state.get_ws_client()->get_client()->get_alog().write(websocketpp::log::alevel::app, "Sequence from heartbeat: " + std::to_string(client_state.get_sequence_counter().value_or(0)));
+//        if (client_state.is_client_connected_to_gateway()) {
+//            auto s = client_state.get_sequence_counter() ? std::to_string(client_state.get_sequence_counter().value()) : "null";
+//            std::stringstream ss;
+//            ss << R"({"op": 1,"d": )" << s << R"(})";
+//            client_state.get_ws_client()->send_message(ss.str());
+//        } else {
+//            client_state.get_ws_client()->get_client()->get_alog().write(websocketpp::log::alevel::debug_close, "Stopping heartbeat");
 //            this->heartbeat_->stop();
-        }
-    });
-    is_running = true;
+//        }
+//    });
+//    is_running = true;
+//}
+
+void hello_handler::start_heartbeat(const discord_client_state &client_state, int interval) const {
+    // Safety check to prevent re-entrance
+    if (is_running) {
+        client_state.get_ws_client()->get_client()->get_alog().write(websocketpp::log::alevel::app, "Attempted to start heartbeat while already running");
+        return;
+    }
+
+    try {
+        heartbeat_ = std::make_unique<heartbeat>(std::chrono::milliseconds(interval), [this, &client_state]() {
+            try {
+                // Your current heartbeat logic here...
+
+                // Check if client is connected
+                if (!client_state.is_client_connected_to_gateway()) {
+                    throw std::runtime_error("Client disconnected from gateway");
+                }
+            } catch (const std::exception& e) {
+                // Log the exception
+                std::stringstream ss;
+                ss << "Error during heartbeat: " << e.what();
+                client_state.get_ws_client()->get_client()->get_alog().write(websocketpp::log::alevel::app, ss.str());
+
+                // Handle the error, for example by stopping the heartbeat
+                heartbeat_->stop();
+                is_running = false;
+
+                // Possibly attempt to reconnect or restart the heartbeat...
+            }
+        });
+
+        is_running = true;
+    } catch (const std::exception& e) {
+        // Log that we failed to start the heartbeat
+        std::stringstream ss;
+        ss << "Failed to start heartbeat: " << e.what();
+        client_state.get_ws_client()->get_client()->get_alog().write(websocketpp::log::alevel::app, ss.str());
+
+        // Reset the heartbeat pointer and the running flag
+        heartbeat_.reset();
+        is_running = false;
+    }
 }
